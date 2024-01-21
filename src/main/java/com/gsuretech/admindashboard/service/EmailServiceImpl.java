02d02a663cf421996157bd211c0d6921fd97707b
Abdulganiyu Abubakar <abdulganiyua@hardcorebiometric.com>
2024-01-21 15:55:53 +0100

    implemented the emailService Interface

---
+++ src/main/java/com/gsuretech/admindashboard/service/EmailServiceImpl.java
@@ -0,0 +1,15 @@
+package com.gsuretech.admindashboard.service;
+
+import com.gsuretech.admindashboard.dto.EmailDetails;
+
+public class EmailServiceImpl implements EmailService{
+    @Override
+    public void sendEmailAlert(EmailDetails emailDetails) {
+
+    }
+
+    @Override
+    public void sendEmailWithAttachment(EmailDetails emailDetails) {
+
+    }
+}

86388d847584a1829d7ec8fdedb4ef5697f96c70
Abdulganiyu Abubakar <abdulganiyua@hardcorebiometric.com>
2024-01-21 16:58:25 +0100

    implemented the emailService class

--- src/main/java/com/gsuretech/admindashboard/service/EmailServiceImpl.java
+++ src/main/java/com/gsuretech/admindashboard/service/EmailServiceImpl.java
@@ -1,11 +1,40 @@
 package com.gsuretech.admindashboard.service;
 
 import com.gsuretech.admindashboard.dto.EmailDetails;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.MailException;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
 
+@Service
+@Slf4j
 public class EmailServiceImpl implements EmailService{
-    @Override
-    public void sendEmailAlert(EmailDetails emailDetails) {
 
+    @Autowired
+    private JavaMailSender javaMailSender;
+
+    @Value(("${spring.mail.username}"))
+    private String senderEmail;
+
+    @Override
+
+    public void sendEmailAlert(EmailDetails emailDetails) {
+        try {
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setFrom(senderEmail);
+            mailMessage.setTo(emailDetails.getRecipient());
+            mailMessage.setText(emailDetails.getMessageBody());
+            mailMessage.setSubject(emailDetails.getSubject());
+
+            javaMailSender.send(mailMessage);
+            log.info("Message sent to; {}", emailDetails.getRecipient());
+            log.info("Message sender: {}", senderEmail);
+        }   catch (MailException e){
+            throw new RuntimeException(e);
+        }
     }
 
     @Override
